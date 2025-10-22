def064c946e269d8764d3c851254492affafe259
mrsbarker <65389492+mrsbarker@users.noreply.github.com>
2025-10-22 14:57:14 -0500

    Refactor validation functions for form inputs

--- appforms.py
+++ appforms.py
@@ -1,19 +1,22 @@
 from wtforms import DecimalField, IntegerField, StringField, SelectField, SubmitField, validators, ValidationError
 from flask_wtf import FlaskForm
 
-def validate_kids(form, field):
-    if field.data != "":
-        if int(field.data) >= 0:
+def validate_nums():
+    def _validating(form, field):
+        if "kid" in field.name:
+            message = "Value for 'Kids' input must be an integer greater than or equal to 0."
+        elif "money" in field.name:
+            message = "Value for 'Salary' input must be a number (without currency symbols and commas)."
+            
+        if field.data == "":
             pass
+        elif field.data != "" and field.data.isnumeric():
+            if "kid" in field.name and int(field.data) < 0:
+                validators.StopValidation()
+                raise ValidationError(message)
         else:
-            raise ValidationError('Input for kids must be an integer')
-        
-def validate_salary(form, field):
-    if field.data != "":
-        if float(field.data) >= 0:
-            pass
-        else:
-            raise ValidationError("Input for salary must be a number")
+            validators.StopValidation()
+            raise ValidationError(message)
 
 
 class MashForm(FlaskForm):
@@ -22,10 +25,10 @@ class MashForm(FlaskForm):
     spouse3 = StringField("Spouse", validators=[validators.Optional()])
     spouse4 = StringField("Spouse", validators=[validators.Optional()])
 
-    kids1 = StringField("Kids", validators=[validators.Length(0, 3), validate_kids, validators.Optional()]) 
-    kids2 = StringField("Kids", validators=[validators.Length(0, 3), validate_kids, validators.Optional()])
-    kids3 = StringField("Kids", validators=[validators.Length(0, 3), validate_kids, validators.Optional()])
-    kids4 = StringField("Kids", validators=[validators.Length(0, 3), validate_kids, validators.Optional()])
+    kids1 = StringField("Kids", validators=[validators.Length(0, 3), validate_nums(), validators.Optional()]) 
+    kids2 = StringField("Kids", validators=[validators.Length(0, 3), validate_nums(), validators.Optional()])
+    kids3 = StringField("Kids", validators=[validators.Length(0, 3), validate_nums(), validators.Optional()])
+    kids4 = StringField("Kids", validators=[validators.Length(0, 3), validate_nums(), validators.Optional()])
 
     job1 = StringField("Career", validators=[validators.Optional()], default="Computer's Choice")
     job2 = StringField("Career", validators=[validators.Optional()], default="Computer's Choice")
@@ -37,10 +40,10 @@ class MashForm(FlaskForm):
     car3 = StringField("Vehicle", validators=[validators.Optional()])
     car4 = StringField("Vehicle", validators=[validators.Optional()])
 
-    money1 = StringField("Salary", validators=[validate_salary, validators.Optional()])
-    money2 = StringField("Salary", validators=[validate_salary, validators.Optional()])
-    money3 = StringField("Salary", validators=[validate_salary, validators.Optional()])
-    money4 = StringField("Salary", validators=[validate_salary, validators.Optional()])
+    money1 = StringField("Salary", validators=[validate_nums(), validators.Optional()])
+    money2 = StringField("Salary", validators=[validate_nums(), validators.Optional()])
+    money3 = StringField("Salary", validators=[validate_nums(), validators.Optional()])
+    money4 = StringField("Salary", validators=[validate_nums(), validators.Optional()])
 
     spun = IntegerField(id="spiral")
 
@@ -67,4 +70,5 @@ class SalaryForm(FlaskForm):
     money = DecimalField("Salary", validators=[validators.DataRequired(), validators.Length(1, 12)])
     #type = "salary"
     submit = SubmitField("Submit")
+
 
